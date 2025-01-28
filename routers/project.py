93ec6924f71d9a13049e42248aade1fb9efdea88
Jack Norton Lovett <jacklovett@mail.com>
2025-01-28 15:56:35 +1000

    Add project search by client first name service

--- routers/project.py
+++ routers/project.py
@@ -1,5 +1,5 @@
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 from schemas.project import Project, ProjectCreate, ProjectUpdate
@@ -29,3 +29,12 @@ def delete_project(project_id: int, database: Session = Depends(get_database)):
 @project_router.get("/", response_model=List[Project])
 def get_projects(database: Session = Depends(get_database)):
     return project_service.get_all(database)
+
+@project_router.get("/search/{first_name}", response_model=List[Project])
+def get_projects_by_first_name(first_name: str, database: Session = Depends(get_database)):
+    projects = project_service.get_projects_by_client_first_name(database, first_name)
+
+    if not projects:
+        raise HTTPException(status_code=404, detail="No projects found for that client")
+
+    return projects
--- services/project.py
+++ services/project.py
@@ -1,6 +1,26 @@
+from sqlalchemy.orm import Session
+
 from crud.project import CRUDProject
+from models import Contact, Client
 from services.base import BaseService
+
 
 class ProjectService(BaseService):
     def __init__(self):
         super().__init__(CRUDProject())
+
+    def get_projects_by_client_first_name(self, database: Session, first_name: str):
+        # Explicitly specify which contact (primary or secondary) to join with
+        clients = database.query(Client) \
+            .join(Contact, Contact.contact_id == Client.primary_contact_id) \
+            .filter(Contact.contact_first_name.ilike(first_name)) \
+            .all()
+
+        if not clients:
+            return []
+
+        # Extract project IDs from the clients associated with the first name
+        project_ids = [client.projects for client in clients]
+
+        # Flatten the list of project IDs and return the projects
+        return [project for sublist in project_ids for project in sublist]
