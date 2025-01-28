fc5b3d5bfff6fb92b6acb45746ff2b0371513ec1
Jack Norton Lovett <jacklovett@mail.com>
2025-01-28 16:00:59 +1000

    Adjust address model

--- models/address.py
+++ models/address.py
@@ -12,9 +12,9 @@ class Address(Base):
     address_suburb = Column(String(45), nullable=False)
     address_city = Column(String(45), nullable=False)
     address_state = Column(String(45), nullable=False)
-    address_postal_code = Column(String(45), nullable=False)
+    address_postal_code = Column(String(4), nullable=False)
     address_country = Column(String(45), nullable=False)
-    address_type = Column(Enum("billing_address", "postal_address"), nullable=False)
+    address_type = Column(Enum("billing_address", "postal_address", "shipping_address", "office_address"), nullable=False)
 
     contacts_as_billing = relationship("Contact", foreign_keys="Contact.address_id", back_populates="billing_address", overlaps="contacts_as_billing")
     contacts_as_postal = relationship("Contact", foreign_keys="Contact.address_id", back_populates="postal_address", overlaps="contacts_as_billing")
