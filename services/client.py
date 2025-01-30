8e900e45cf295cd99eaf1230b2154e35b37db266
Jack Norton Lovett <jacklovett@mail.com>
2025-01-30 15:50:48 +1000

    Add enrichment services for tables

--- services/client.py
+++ services/client.py
@@ -1,7 +1,37 @@
 from crud.client import CRUDClient
 from services.base import BaseService
+from services.contact import ContactService
 
 
 class ClientService(BaseService):
     def __init__(self):
         super().__init__(CRUDClient())
+
+    def enrich_client(self, database, client):
+        """Convert foreign keys into meaningful values for display."""
+        client_dict = client.__dict__.copy()
+
+        contact_service = ContactService()
+
+        # Fetch primary contact details
+        primary_contact = contact_service.get_by_id(database, client.primary_contact_id)
+        if primary_contact:
+            client_dict['primary_contact_name'] = (
+                f"{primary_contact.contact_first_name} {primary_contact.contact_last_name}"
+            )
+            client_dict['primary_contact_email'] = primary_contact.contact_email or "N/A"
+            client_dict['primary_contact_phone'] = primary_contact.contact_phone or "N/A"
+        else:
+            client_dict['primary_contact_name'] = "Unknown Contact"
+            client_dict['primary_contact_email'] = "N/A"
+            client_dict['primary_contact_phone'] = "N/A"
+
+        # Count associated projects
+        client_dict['project_count'] = len(client.projects)
+
+        return client_dict
+
+    def get_enriched_clients(self, database):
+        """Retrieve all clients with enriched values."""
+        clients = self.get_all(database)
+        return [self.enrich_client(database, client) for client in clients]
--- services/project.py
+++ services/project.py
@@ -1,26 +1,42 @@
-from sqlalchemy.orm import Session
-
 from crud.project import CRUDProject
-from models import Contact, Client
+from services.address import AddressService
 from services.base import BaseService
+from services.client import ClientService
+from services.contact import ContactService
 
 
 class ProjectService(BaseService):
     def __init__(self):
         super().__init__(CRUDProject())
 
-    def get_projects_by_client_first_name(self, database: Session, first_name: str):
-        # Explicitly specify which contact (primary or secondary) to join with
-        clients = database.query(Client) \
-            .join(Contact, Contact.contact_id == Client.primary_contact_id) \
-            .filter(Contact.contact_first_name.ilike(first_name)) \
-            .all()
+    def enrich_project(self, database, project):
+        """Convert foreign keys into meaningful values for display."""
+        project_dict = project.__dict__.copy()
 
-        if not clients:
-            return []
+        client_service = ClientService()
+        contact_service = ContactService()
+        address_service = AddressService()
 
-        # Extract project IDs from the clients associated with the first name
-        project_ids = [client.projects for client in clients]
+        # Fetch client details
+        client = client_service.get_by_id(database, project.client_id)
+        if client:
+            primary_contact = contact_service.get_by_id(database, client.primary_contact_id)
+            project_dict[
+                'client_name'] = f"{primary_contact.contact_first_name} {primary_contact.contact_last_name}" if primary_contact else "Unknown Client"
+        else:
+            project_dict['client_name'] = "Unknown Client"
 
-        # Flatten the list of project IDs and return the projects
-        return [project for sublist in project_ids for project in sublist]
+        # Fetch address details
+        address = address_service.get_by_id(database, project.address_id)
+        if address:
+            project_dict[
+                'full_address'] = f"{address.address_street}, {address.address_suburb}, {address.address_city}, {address.address_state} {address.address_postal_code}"
+        else:
+            project_dict['full_address'] = "Unknown Address"
+
+        return project_dict
+
+    def get_enriched_projects(self, database):
+        """Retrieve all projects with enriched values."""
+        projects = self.get_all(database)
+        return [self.enrich_project(database, project) for project in projects]
